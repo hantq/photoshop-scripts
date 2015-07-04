a7d41db28e5a059fe971a02a20562d3d1152e2c9
hantq <simon.hon.90@gmail.com>
2015-07-04 16:12:06 +0800

    add exportRetinaPNG2x and exportRetinaPNG3x.

---
+++ exportRetinaPNG2x.jsx
@@ -0,0 +1,27 @@
+var doc = app.activeDocument;
+var docName = doc.name.replace(/\.[^\.]+$/, '');
+var docPath = doc.path;
+var scale = "200%";
+var extensionName = "@2x.png";
+
+function exportScaledPNG() {
+    doc.resizeImage(scale, scale, doc.resolution, ResampleMethod.BICUBIC);
+    pngFile = File(docPath + "/" + docName + extensionName);
+    if (pngFile.exists) {
+        pngFile.remove();
+    }
+    pngSaveOptions = new PNGSaveOptions();
+    pngSaveOptions.compression = 9;
+    doc.saveAs(pngFile, pngSaveOptions, true, Extension.LOWERCASE);
+    doc.activeHistoryState = doc.historyStates[doc.historyStates.length - 2];
+    app.purge(PurgeTarget.HISTORYCACHES);
+    doc.save();
+}
+
+try {
+	exportScaledPNG();
+} catch (e) {
+    if (DialogModes.NO != app.playbackDisplayDialogs) {
+        alert(e + " : " + e.line);
+    }
+}
